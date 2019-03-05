f68331d63a6810857c7957355f918f31af89b8db
Colin Targonski <ctargon@g.clemson.edu>
2019-03-04 20:15:48 -0500

    heatmap update

--- utils/heatmap.py
+++ utils/heatmap.py
@@ -1,10 +1,13 @@
 import numpy as np 
+import pandas as pd
 import matplotlib.pyplot as plt 
 import matplotlib.colors as mcol
 import matplotlib.cm as cm
 
+from utils import read_subset_file
 
-def heatmap(arr, titles=[r"$X$", r"$P$", r"$X_{adv}$", r"$\mu_{T}$"]):
+
+def heatmap(arr, genes, titles=[r"$X$", r"$P$", r"$X_{adv}$", r"$\mu_{T}$"]):
 	fig, ax = plt.subplots(1, 4)
 
 	# Make a user-defined colormap.
@@ -43,12 +46,12 @@ def heatmap(arr, titles=[r"$X$", r"$P$", r"$X_{adv}$", r"$\mu_{T}$"]):
 
 		if i == 0:
 			ax[i].set_yticks(np.arange(arr.shape[1]))
-			ax[i].set_yticklabels(np.arange(arr.shape[1]) + 1)
+			ax[i].set_yticklabels(genes)
 		else:
 			ax[i].set_yticks([])
 			ax[i].set_yticklabels([])
 
-	cbar = ax[i].figure.colorbar(im, ax=ax, shrink=0.5)
+	cbar = ax[i].figure.colorbar(im, ax=ax[i], shrink=0.5)
 	cbar.ax.set_ylabel("Expression Level", rotation=-90, va="bottom")
 
 	plt.show()
@@ -80,7 +83,39 @@ if __name__ == "__main__":
 
 	results = np.vstack([x, p , x_hat, mu_t])
 
-	heatmap(results)
+	# get list of genes
+	total_gene_list = np.load("./data/tissue/gtex_gene_list_v7.npy")
+	subsets = read_subset_file("./data/subsets/hallmark_experiments.txt")
+
+	tot_genes = []
+	missing_genes = []
+
+	print('checking for valid genes...')
+	for s in subsets:
+		genes = []
+		for g in subsets[s]:
+			if g not in tot_genes:
+				tot_genes.append(g)
+			if g in total_gene_list:
+				genes.append(g)
+			else:
+				if g not in missing_genes:
+					missing_genes.append(g)
+		subsets[s] = genes
+				#print('missing gene ' + str(g))
+	print('missing ' + str(len(missing_genes)) + '/' + str(len(tot_genes)) + ' genes' + ' or ' \
+		 + str(int((float(len(missing_genes)) / len(tot_genes)) * 100.0)) + '% of genes')
+
+	genes = subsets["HALLMARK_HEDGEHOG_SIGNALING"]
+	names = ["X", "P", "X_adv", "mu_T"]
+
+	# create dataframe with gene names and 4 vectors
+	df = pd.DataFrame(data=results.T, columns=names, index=genes)
+
+	# sort dataframe by perturbation
+	df = df.sort_values("P", ascending=False)
+
+	heatmap(df.values.T, df.index.values.tolist())
 
 
 
