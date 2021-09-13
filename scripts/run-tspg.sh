8571a226962fd6d2ad98a5e4f01f55da25519575
Ben Shealy <bentshermann@gmail.com>
2021-09-13 15:49:36 -0400

    Worked on example run script

--- scripts/run-tspg.sh
+++ scripts/run-tspg.sh
@@ -3,6 +3,10 @@
 #PBS -l select=1:ncpus=2:ngpus=1:mem=64gb:gpu_model=p100,walltime=72:00:00
 #PBS -j oe
 
+set -e
+
+export TF_CPP_MIN_LOG_LEVEL="3"
+
 # This should be the directory where you cloned the TSPG repository
 TSPG_DIR="${HOME}/TSPG"
 
@@ -20,7 +24,7 @@ OUTPUT_DIR="output"
 # use conda environment... look at TSPG documentation, and make a conda environment named 'tspg'
 # ...or change this environment name here 
 module purge
-module load anaconda3/5.1.0
+module load anaconda3/5.1.0-gcc
 
 source activate tspg
 
@@ -28,14 +32,11 @@ source activate tspg
 # Use your home and zfs directory for permanent storage only
 cp -r ${INPUT_DIR} ${TMPDIR}
 
-# remove old output data
-# rm -rf ${OUTPUT_DIR}
-
+# train target model on a gene set
 echo
 echo "PHASE 1: TRAIN TARGET MODEL"
 echo
 
-# train target model on a gene set
 ${TSPG_DIR}/bin/train-target.py \
     --dataset    ${TMPDIR}/${TRAIN_DATA} \
     --labels     ${TMPDIR}/${TRAIN_LABELS} \
@@ -43,24 +44,25 @@ ${TSPG_DIR}/bin/train-target.py \
     --set        ${GENE_SET} \
     --output-dir ${TMPDIR}/${OUTPUT_DIR}
 
+# train AdvGAN model on a gene set
 echo
 echo "PHASE 2: TRAIN PERTURBATION GENERATOR"
 echo
 
-# train AdvGAN model on a gene set
 ${TSPG_DIR}/bin/train-advgan.py \
     --dataset    ${TMPDIR}/${TRAIN_DATA} \
     --labels     ${TMPDIR}/${TRAIN_LABELS} \
     --gene-sets  ${TMPDIR}/${GMT_FILE} \
     --set        ${GENE_SET} \
     --target     ${TARGET_CLASS} \
+    --target-cov full \
     --output-dir ${TMPDIR}/${OUTPUT_DIR}
 
+# generate perturbed samples using AdvGAN model
 echo
 echo "PHASE 3: GENERATE SAMPLE PERTURBATIONS"
 echo
 
-# generate perturbed samples using AdvGAN model
 ${TSPG_DIR}/bin/perturb.py \
     --train-data      ${TMPDIR}/${TRAIN_DATA} \
     --train-labels    ${TMPDIR}/${TRAIN_LABELS} \
@@ -72,11 +74,11 @@ ${TSPG_DIR}/bin/perturb.py \
     --output-dir      ${TMPDIR}/${OUTPUT_DIR}
 
 
+# create t-SNE and heatmap visualizations of perturbed samples for a gene set
 echo
 echo "PHASE 4: VISUALIZE SAMPLE PERTURBATIONS"
 echo
 
-# create t-SNE and heatmap visualizations of perturbed samples for a gene set
 ${TSPG_DIR}/bin/visualize.py \
     --train-data      ${TMPDIR}/${TRAIN_DATA} \
     --train-labels    ${TMPDIR}/${TRAIN_LABELS} \
@@ -89,4 +91,6 @@ ${TSPG_DIR}/bin/visualize.py \
     --tsne \
     --heatmap
 
+# save output data to permanent storage
+rm -rf ${OUTPUT_DIR}
 cp -r ${TMPDIR}/${OUTPUT_DIR} .
